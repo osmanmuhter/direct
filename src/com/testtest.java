4218f1e87d6c25828da22c4ed494cb262309660e
osmanmuhter <osman.muhter@gmail.com>
2019-10-15 19:07:56 -0700

    I changed chrome to firefox and changed the url

--- src/com/testtest.java
+++ src/com/testtest.java
@@ -2,15 +2,16 @@ package com;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class testtest {
     public static void main(String[] args) {
-       System.setProperty("webdriver.chrome.driver",
-               "/Users/osman/Documents/selenium dependencies /drivers/chromedriver");
+       System.setProperty("webdriver.gecko.driver",
+               "/Users/osman/Documents/selenium dependencies /drivers/geckodriver");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
 
-        driver.get("https://google.com");
+        driver.get("https://youtube.com");
 
     }
 }
